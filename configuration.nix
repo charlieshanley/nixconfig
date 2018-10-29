33df85c66f0969d8d83ba5cb8c377d97d6f34ef2
Charlie Hanley <charles.scott.hanley+git@gmail.com>
2018-10-29 18:54:44 -0400

    added git alias lg to home-manager

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,7 @@
       enable = true;
       userName = "Charlie Hanley";
       userEmail = "charles.scott.hanley+git@gmail.com";
+      aliases.lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
     };
   };
 
