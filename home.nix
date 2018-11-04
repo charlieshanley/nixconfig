427131c9f8fcb15fb19f5e348ed7618ba7875ac6
Charlie Hanley <charles.scott.hanley+git@gmail.com>
2018-11-04 11:21:43 -0500

    minimal configuration for nvim and vim for comparison

--- home.nix
+++ home.nix
@@ -37,7 +37,6 @@ in
       bash = {
         enable = true;
         initExtra = ''
-        source ${gruvbox-phaazon}/share/vim-plugins/gruvbox/gruvbox_256palette.sh
         cs() { cd "$@" && ls -lat; }
         '';
       };
@@ -51,64 +50,25 @@ in
         enable = true;
         configure = {
           packages.myVimPackage = with pkgs.vimPlugins; {
-            start = [
-              gruvbox-phaazon
-              gitgutter airline commentary
-              vim-nix haskell-vim
-            ];
-            opt = [];
+            start = [ ];
+            opt = [ haskell-vim ];
           };
-          customRC = ''
+	  customRC = ''
 	    set nocompatible
-	    syntax on
 	    filetype plugin indent on
-	    set hidden
-
-            " for gitgutter
-	    set updatetime=300 
-
-            let g:gruvbox_italic=1
-	    colorscheme gruvbox
-	    set background=dark
-            highlight Normal ctermbg=None
-
-	    set wildmenu
-            set showcmd
-	    set hlsearch
-	    set incsearch
-	    set ignorecase
-	    set smartcase
-	    set backspace=indent,eol,start
-	    set autoindent
-	    set nostartofline
-	    set ruler
-	    set laststatus=2
-	    set confirm
-
-	    set number relativenumber
-
-	    " revert to absolute numbers in insert mode and when buffer loses focus
-	    augroup numbertoggle
-	      autocmd!
-	      autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
-	      autocmd BufLeave,FocusLost,InsertEnter   * set norelativenumber
-	    augroup end
-
-	    set notimeout ttimeout ttimeoutlen=200
-
-	    " <F11> to toggle between paste and nopaste
-	    set pastetoggle=<F11>
-	    set scrolloff=3
-	    set shiftwidth=4
-	    set softtabstop=4
-	    set expandtab
-	    set colorcolumn=80
-
-	    let mapleader=" "
-	    map Y y$
-	    noremap <C-L> :nohl<CR><C-L>
-          '';
+	    syntax on
+	    packadd haskell-vim
+	  '';
         };
+      };
+      vim = {
+        enable = true; 
+	plugins = [ "haskell-vim" ];
+	extraConfig = ''
+          set nocompatible
+	  filetype plugin indent on
+	  syntax on
+	'';
       };
     };
   };
